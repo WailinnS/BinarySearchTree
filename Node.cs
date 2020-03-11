c8db9ac09acbf3c726f8180fb0f6c7ad1394417d
WailinnS <Wailinnsaw@gmail.com>
2020-03-10 19:36:52 -0700

    Add project files.

---
+++ Node.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> LeftChild { get; set; }
+        public Node<T> RightChild { get; set; }
+        public Node<T> Parent { get; set; }
+
+        public int ChildCount { get; set; }
+        public bool IsLeftChild
+        {
+            get
+            {
+                return Parent == null ? false : Parent.LeftChild == this;
+            }
+        }
+        public bool IsRightChild
+        {
+            get
+            {
+                return Parent == null ? false : Parent.RightChild == this;
+            }
+        }
+
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            /*
+                    Node:
+                    -Value
+                    -LeftChild
+                    -RightChild
+                    -Parent
+
+                    -IsLeftChild
+                    -IsRightChild
+                    -ChildCount - count how many children
+            */
+        }
+    }
+}
